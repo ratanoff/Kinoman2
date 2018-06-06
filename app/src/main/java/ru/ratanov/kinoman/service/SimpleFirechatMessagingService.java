2643ad2d53c3b05febd1b670ee79e14f62328b06
ratanov-lv <ratanov-lv@bisys.ru>
2018-06-06 17:49:23 +0500

    firebase notifications

---
+++ app/src/main/java/ru/ratanov/kinoman/service/SimpleFirechatMessagingService.java
@@ -0,0 +1,12 @@
+package ru.ratanov.kinoman.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class SimpleFirechatMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+    }
+}
